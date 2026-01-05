a4a0a17d429b3fa3ec2e3e5f9108e5bf9545c972
Zuraw7 <zurawicki123@gmail.com>
2026-01-05 15:50:54 +0100

    Changed textures from LINEAR to NEAREST

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -5,7 +5,7 @@
 #include <glm/ext/matrix_transform.hpp>
 #include "../utils/declarations.hpp"
 
-Camera::Camera(const glm::vec3 position, const glm::vec3 target, const glm::vec3 worldUp) : m_camPosition(position), m_targetPos(target), m_worldUp(worldUp), m_yaw(-90.0f), m_pitch(0.0f), m_speed(1.0f), m_fov(80.0f) {
+Camera::Camera(const glm::vec3 position, const glm::vec3 target, const glm::vec3 worldUp) : m_camPosition(position), m_targetPos(target), m_worldUp(worldUp), m_yaw(-90.0f), m_pitch(0.0f), m_speed(3.0f), m_fov(80.0f) {
     updateCameraVectors();
     m_view = lookAt();
     m_projection = glm::perspective(glm::radians(m_fov), Resolution::aspect, NEAR, FAR);
--- src/textures/Texture2D.cpp
+++ src/textures/Texture2D.cpp
@@ -14,11 +14,11 @@ std::shared_ptr<Texture2D> Texture2D::create(const std::string &path) {
     glGenTextures(1, &texture->m_id);
     glBindTexture(GL_TEXTURE_2D, texture->m_id);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     const int format = (texture->m_nrChannels == 4) ? GL_RGBA : GL_RGB;
     glTexImage2D(GL_TEXTURE_2D, 0, format, texture->m_width, texture->m_height, 0, format, GL_UNSIGNED_BYTE, data);
